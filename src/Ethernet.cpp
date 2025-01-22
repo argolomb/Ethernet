0ef43996b8ebfecaacc12cace7f410bb326a6c2e
Miguel Argolo <argolo.mb@gmail.com>
2025-01-22 16:27:14 -0300

    change to work udp packages  and arp requests

--- src/Ethernet.cpp
+++ src/Ethernet.cpp
@@ -83,10 +83,13 @@ void EthernetClass::begin(uint8_t *mac, IPAddress ip, IPAddress dns, IPAddress g
 	if (W5100.init() == 0) return;
 	SPI.beginTransaction(SPI_ETHERNET_SETTINGS);
 	W5100.setMACAddress(mac);
+	Serial.print("Begin");
+	Serial.print(ARDUINO);
 #if ARDUINO > 106 || TEENSYDUINO > 121
-	W5100.setIPAddress(ip._address.bytes);
+	W5100.setIPAddress(ip.raw_address());
 	W5100.setGatewayIp(gateway._address.bytes);
 	W5100.setSubnetMask(subnet._address.bytes);
+
 #else
 	W5100.setIPAddress(ip._address);
 	W5100.setGatewayIp(gateway._address);
